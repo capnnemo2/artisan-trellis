24adb07117ab9cf40a2d5f21a2a3b617bea2f0ae
Ben Hernandez <benjammin234@gmail.com>
2020-12-08 10:06:58 -0800

    Combined state hooks into single hook w/object

--- src/Site1/Site1.js
+++ src/Site1/Site1.js
@@ -4,24 +4,25 @@ import "./Site1.css";
 import ReactTooltip from "react-tooltip";
 import MutantDetails from "../MutantDetails/MutantDetails";
 
-// TODO combine state into single, combined line
-
 function Site1() {
   const [mutant_details, set_mutant_details] = useState(false);
-  //   const [[m_name, m_trait], [set_m_name, set_m_trait]] = useState("");
-  const [m_name, set_m_name] = useState("");
-  const [m_trait, set_m_trait] = useState("");
+  const [mutant, set_mutant] = useState({
+    m_name: "",
+    m_trait: "",
+    m_team: "",
+  });
 
-  function handleMutantClick(name, trait) {
-    if (name !== m_name) {
-      set_m_name(name);
+  function handleMutantClick(name, trait, team) {
+    if (name !== mutant.m_name) {
+      set_mutant((prevState) => ({
+        ...prevState,
+        m_name: name,
+        m_trait: trait,
+        m_team: team,
+      }));
       set_mutant_details(true);
-    } else if (name === m_name) {
-      mutant_details ? set_mutant_details(false) : set_mutant_details(true);
-    }
-
-    if (trait !== m_trait) {
-      set_m_trait(trait);
+    } else if (name === mutant.m_name) {
+      set_mutant_details((mutant_details) => !mutant_details);
     }
   }
 
@@ -29,34 +30,36 @@ function Site1() {
     <div className="Site1">
       <p
         className="mutant magneto"
-        data-tip="Magneto<br />Villain<br />Brains"
-        data-multiline="true"
+        data-tip="Magneto"
         onClick={(e) => {
           e.preventDefault();
-          handleMutantClick("magneto", "brains");
+          handleMutantClick("magneto", "brains", "supervillain");
         }}
       ></p>
       <p
         className="mutant juggernaut"
-        data-tip="Juggernaut <br /> Villain <br /> Brawn"
-        data-multiline="true"
+        data-tip="Juggernaut"
         onClick={(e) => {
           e.preventDefault();
-          handleMutantClick("juggernaut", "brawn");
+          handleMutantClick("juggernaut", "brawn", "supervillain");
         }}
       ></p>
       <p
         className="mutant cyclops"
-        data-tip="Cyclops <br /> Hero <br /> Brains"
+        data-tip="Cyclops <br /> Sexy"
         data-multiline="true"
         onClick={(e) => {
           e.preventDefault();
-          handleMutantClick("cyclops", "brains");
+          handleMutantClick("cyclops", "brains", "superhero");
         }}
       ></p>
       {mutant_details ? (
         <div className="mutant_details">
-          <MutantDetails m_name={m_name} m_trait={m_trait} />
+          <MutantDetails
+            m_name={mutant.m_name}
+            m_trait={mutant.m_trait}
+            m_team={mutant.m_team}
+          />
         </div>
       ) : (
         ""
