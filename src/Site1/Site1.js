6c7d368c4d9afe80080591d3b5ac8c60da0bc3d0
Ben Hernandez <benjammin234@gmail.com>
2020-12-08 08:44:18 -0800

    Added onClick handler to tooltips

--- src/Site1/Site1.js
+++ src/Site1/Site1.js
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Site1.css";
 
 import ReactTooltip from "react-tooltip";
 
 function Site1() {
+  const [mutant_details, set_mutant_details] = useState(false);
+
+  function handleMutantClick() {
+    mutant_details ? set_mutant_details(false) : set_mutant_details(true);
+  }
+
   return (
     <div className="Site1">
-      <p className="villain magneto" data-tip="Magneto"></p>
-      <p className="villain juggernaut" data-tip="Juggernaut"></p>
-      <p className="villain cyclops" data-tip="Cyclops"></p>
+      <p
+        className="mutant magneto"
+        data-tip="Magneto<br />Villain<br />Brains"
+        data-multiline="true"
+        onClick={handleMutantClick}
+      ></p>
+      <p
+        className="mutant juggernaut"
+        data-tip="Juggernaut <br /> Villain <br /> Brawn"
+        data-multiline="true"
+        onClick={handleMutantClick}
+      ></p>
+      <p
+        className="mutant cyclops"
+        data-tip="Cyclops <br /> Hero <br /> Brains"
+        data-multiline="true"
+        onClick={handleMutantClick}
+      ></p>
+      {mutant_details ? (
+        <div className="mutant_details">
+          <p>MUTANT</p>
+        </div>
+      ) : (
+        ""
+      )}
+
       <ReactTooltip place="top" type="dark" effect="solid" />
     </div>
   );
