324d1fc9cd0a84bfc0ade5645b2eb59049fcde0b
Ben Hernandez <benjammin234@gmail.com>
2020-12-09 15:53:46 -0800

    Site3 framework better fleshed out

--- src/MutantDetails/MutantDetails.js
+++ src/MutantDetails/MutantDetails.js
@@ -3,10 +3,21 @@ import "./MutantDetails.css";
 
 function MutantDetails({ m_name, m_trait, m_team }) {
   return (
-    <div className="MutantDetails">
-      <p>{m_name}</p>
-      <p>{m_team}</p>
-      <p>{m_trait}</p>
+    <div>
+      <div className="MutantDetails">
+        <p>{m_name}</p>
+        <p>Dimensions: {m_team}</p>
+        <p>Product Code: {m_trait}</p>
+      </div>
+      <div className="alt-prods">
+        <ul>
+          <li>
+            <i>Similar products</i>
+          </li>
+          <li>sg</li>
+          <li>lg</li>
+        </ul>
+      </div>
     </div>
   );
 }
--- src/Site3/Site3.js
+++ src/Site3/Site3.js
@@ -1,41 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Site3.css";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
 import ReactTooltip from "react-tooltip";
+import MutantDetails from "../MutantDetails/MutantDetails";
 
 // TODO footer isnt sticky
 
 export default function Site3() {
+  const [product, set_product] = useState(false);
+  const [product_details, set_product_details] = useState({
+    p_name: "",
+    p_code: "",
+    p_dimensions: "",
+  });
+
+  function handleClickProduct(name, code, dimensions) {
+    if (name !== product_details.p_name) {
+      set_product_details((prevState) => ({
+        ...prevState,
+        p_name: name,
+        p_code: code,
+        p_dimensions: dimensions,
+      }));
+      set_product(true);
+    } else if (name === product_details.p_name) {
+      set_product((product) => !product);
+    }
+  }
+
   return (
     <div className="Site3">
       <Nav />
       <div className="content-details">
-        <ul>
-          <li>alpha</li>
-          <li>something</li>
-          <li>omega</li>
-        </ul>
+        <div>
+          {product ? (
+            <div className="mutant_details">
+              <MutantDetails
+                m_name={product_details.p_name}
+                m_trait={product_details.p_code}
+                m_team={product_details.p_dimensions}
+              />
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
       </div>
       <div className="product-nav">
-        <ul>
-          <li>rock garden</li>
-          <li>zen garden</li>
-          <li>hippy garden</li>
-        </ul>
+        <div></div>
       </div>
       <div className="content-window">
-        <h2>content</h2>
-        <p>
-          this is a lot of content so that the grid will work better, maybe,
-          hopefully
-        </p>
-        <p>Here is some more content</p>
-        <p>Here is some more content</p>
-        <p>Here is some more content</p>
-        <p>Here is some more content</p>
-        <p>Here is some more content</p>
+        <div className="content-window-img">
+          <div
+            className="embedded_product med-g"
+            data-tip="Medium Globe"
+            onClick={(e) => {
+              e.preventDefault();
+              handleClickProduct("Medium Globe", "MG", "24in x 12in");
+            }}
+          ></div>
+          <ReactTooltip place="top" type="dark" effect="solid" />
+        </div>
       </div>
+
       <Footer />
     </div>
   );
